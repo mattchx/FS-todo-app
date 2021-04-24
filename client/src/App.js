203df1e0be866f98afc8f258b1798a54ac3461c2
Matt <mattcharlesh@gmail.com>
2021-04-23 21:06:54 -0400

    added search form with button

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  Center,
   theme,
+  FormControl,
+  FormLabel,
+  Box,
+  FormHelperText,
+  Input,
+  Button,
+  Flex,
+  useStyleConfig,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <ColorModeSwitcher />
-      </Box>
+      <Center>
+        <Box width={500}>
+          <ColorModeSwitcher />
+          <FormControl id="email">
+            <FormLabel>To Do</FormLabel>
+            <Flex>
+              <Input type="text" placeholder="What needs to be done?" />
+              <Button colorScheme="teal">Add</Button>
+            </Flex>
+          </FormControl>
+        </Box>
+      </Center>
     </ChakraProvider>
   );
 }
 
 export default App;
+
+//border-radius: 15px 50px 30px 5px
