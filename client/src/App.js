84c1c2f3b4a461ed62a8fbd3e6540f8dca620535
Matt <mattcharlesh@gmail.com>
2021-04-25 17:00:58 -0400

    handled rendering of complete todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
 import { useEffect, useState } from 'react';
-import {
-  ChakraProvider,
-  Center,
-  theme,
-  Box,
-  
-} from '@chakra-ui/react';
+import { ChakraProvider, Center, theme, Box, chakra } from '@chakra-ui/react';
 import axios from 'axios';
 import InputTodo from './components/InputTodo';
 import ListTodos from './components/ListTodos';
@@ -21,6 +15,7 @@ function App() {
 
   useEffect(() => {
     fetchTodos();
+    console.log(todos);
   }, []);
 
   const handleAddTodo = () => {
@@ -31,13 +26,29 @@ function App() {
     setTodos(todos.filter(todo => todo.todo_id !== id));
   };
 
+  const handleCompletedTodo = (id, isComplete) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.todo_id === id) {
+          return { ...todo, complete: !isComplete };
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
     <ChakraProvider theme={theme}>
       <Center>
         <Box width={500}>
           <InputTodo handleAddTodo={handleAddTodo} />
           <Center>
-            <ListTodos todos={todos} handleDeleteTodo={handleDeleteTodo} />
+            
+            <ListTodos
+              todos={todos}
+              handleDeleteTodo={handleDeleteTodo}
+              handleCompletedTodo={handleCompletedTodo}
+            />
           </Center>
         </Box>
       </Center>
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,12 +1,26 @@
 import { Fragment } from 'react';
-import { Divider, Flex, Button, Spacer, Box } from '@chakra-ui/react';
+import { Divider, Flex, Button, Spacer, Box, Text } from '@chakra-ui/react';
 import axios from 'axios';
+import styled from '@emotion/styled';
 
-const ListTodo = ({todos, handleDeleteTodo}) => {
+const ListTodo = ({ todos, handleDeleteTodo, handleCompletedTodo }) => {
   const deleteTodo = async id => {
     try {
       await axios.delete(`http://localhost:5000/todos/${id}`);
       handleDeleteTodo(id);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  const todoIsComplete = async (id, complete) => {
+    // console.log(e);
+    const isComplete = complete ? 'false' : 'true';
+    // console.log(id);
+    try {
+      await axios.put(`http://localhost:5000/todos/${id}`, {
+        complete: isComplete,
+      });
+      handleCompletedTodo(id, complete);
     } catch (err) {
       console.log(err.message);
     }
@@ -19,15 +33,33 @@ const ListTodo = ({todos, handleDeleteTodo}) => {
           return (
             <Fragment key={item.todo_id}>
               <Flex mt={3}>
-                <p>{item.description}</p>
-                <Spacer />
-                <Button
-                  onClick={() => deleteTodo(item.todo_id)}
-                  size="xs"
-                  colorScheme="red"
-                >
-                  Delete
-                </Button>
+                {!item.complete ? (
+                    <>
+                  <P
+                    onClick={() => todoIsComplete(item.todo_id, item.complete)}
+                  >
+                    {item.description}
+                  </P>
+                  <Spacer />
+                  <Button
+                    onClick={() => deleteTodo(item.todo_id)}
+                    size="xs"
+                    colorScheme="red"
+                  >
+                    Delete
+                  </Button>
+                  </>
+                ) : (
+                  <P
+                    complete
+                    color="gray"
+                    as="s"
+                    onClick={() => todoIsComplete(item.todo_id, item.complete)}
+                  >
+                    {item.description}
+                  </P>
+                )}
+                
               </Flex>
               <Divider m={3} />
             </Fragment>
@@ -38,3 +70,13 @@ const ListTodo = ({todos, handleDeleteTodo}) => {
 };
 
 export default ListTodo;
+
+const P = styled.p`
+  color: ${props => (props.complete ? 'grey' : 'black')};
+  text-decoration: ${props => (props.complete ? 'line-through' : 'none')};
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+
