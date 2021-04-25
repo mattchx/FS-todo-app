9eb38b472568c1f216f9a731f6b49cfb948c4d47
Matt <mattcharlesh@gmail.com>
2021-04-25 12:04:52 -0400

    Update ListTodos.js
    divider styling

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
+import { Divider } from '@chakra-ui/react';
 import axios from 'axios';
 const ListTodo = () => {
   const [todos, setTodos] = useState(null);
@@ -13,9 +14,19 @@ const ListTodo = () => {
     fetchTodos();
   }, []);
 
-  return <div>{todos && todos.map(item => {
-      return <p key={item.todo_id}>{item.description}</p>
-  }) }</div>;
+  return (
+    <div>
+      {todos &&
+        todos.map(item => {
+          return (
+            <Fragment key={item.todo_id}>
+              <p>{item.description}</p>
+              <Divider />
+            </Fragment>
+          );
+        })}
+    </div>
+  );
 };
 
 export default ListTodo;
