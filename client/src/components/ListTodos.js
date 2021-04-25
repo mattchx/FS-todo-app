40b3c3a7f946414970cbdb8ed735b05b4042f1e0
Matt <mattcharlesh@gmail.com>
2021-04-25 14:39:40 -0400

    Update ListTodos.js
    - added delete item handler

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,12 +1,13 @@
 import { useEffect, useState, Fragment } from 'react';
 import { Divider, Flex, Button, Spacer, Box } from '@chakra-ui/react';
 import axios from 'axios';
+
 const ListTodo = () => {
-  const [todos, setTodos] = useState(null);
+  const [todos, setTodos] = useState([]);
 
   const fetchTodos = async () => {
     const res = await axios.get('http://localhost:5000/todos');
-    console.log(res.data);
+    //console.log(res.data);
     setTodos(res.data);
   };
 
@@ -14,21 +15,35 @@ const ListTodo = () => {
     fetchTodos();
   }, []);
 
+  const deleteTodo = async id => {
+    console.log(typeof id);
+    try {
+      await axios.delete(`http://localhost:5000/todos/${id}`);
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <Box minW={300}>
       {todos &&
         todos.map(item => {
           return (
-            <Fragment key={item.todo_id} >
+            <Fragment key={item.todo_id}>
               <Flex mt={3}>
                 <p>{item.description}</p>
                 <Spacer />
-                <Button size="xs" colorScheme="red">
+                <Button
+                  onClick={() => deleteTodo(item.todo_id)}
+                  size="xs"
+                  colorScheme="red"
+                >
                   Delete
                 </Button>
               </Flex>
-              <Divider m={3}/>
-            </Fragment >
+              <Divider m={3} />
+            </Fragment>
           );
         })}
     </Box>
